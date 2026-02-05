d64ac125f36d445818da5614cda9bbbd9fb52a83
onyangodonomondi <onyangodonomondi@gmail.com>
2026-02-05 14:10:11 +0300

    security: remove exposed secrets from tests.py

--- cyberpanel/IncBackups/tests.py
+++ cyberpanel/IncBackups/tests.py
@@ -4,8 +4,9 @@ import configparser
 CurrentContent = """[usman]
 type = sftp
 host = staging.cyberpanel.net
-user = abcds2751
-pass = s0RBbJU8EhfQ-wvFgbOVEmy3HK6y19A
+user = example_user
+pass = PLACEHOLDER_PASSWORD
+
 shell_type = unix
 md5sum_command = md5sum
 sha1sum_command = sha1sum
@@ -17,7 +18,7 @@ client_secret = ""
 scope = drive
 root_folder_id = ""
 service_account_file = ""
-token = {"access_token":"ya29.a0Ael9sCPUpwAZpHChyBkAYrDGo5BRjkj2OV1r9KNBXdXZZrjTrjPOHxTkayEr-hfKNhsqYrvChowxQw-EgTO7JobBE7IrZpLDpdpEOTY49JOg-PagtPLU_TuqFPab356TdeC0-f2RHQ_2arU1pN92aKcgfp7CaCgYKASESARESFQF4udJhaS1_8FVFFkG-ds0yPY0APA0163","token_type":"Bearer","refresh_token":"1//09Sgboc4b9-kYCgYIARAAGAkSNgF-L9IrgxJ3jKcd0UDraNAncWDKRUNu0L5ORiaS8H_QaXv2y85p0cL3ZArEaShSxy2P_Kb0CQ"}
+token = {"access_token":"PLACEHOLDER_ACCESS_TOKEN","token_type":"Bearer","refresh_token":"PLACEHOLDER_REFRESH_TOKEN"}
 """
 
 # Read the configuration string
